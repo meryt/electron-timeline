a2b16fd7d2da5dbea0431a685d144ff79e7509d8
Jenny van Hoof <jvanhoof@xmatters.com>
2018-01-05 15:28:10 -0800

    Create an example timeline

--- renderer.js
+++ renderer.js
@@ -1,2 +1,12 @@
 
-document.getElementById('timeline').innerHTML = 'Timeline goes here'
+const vis = require('vis')
+
+let timelineDiv = document.getElementById('timeline')
+
+let items = new vis.DataSet([
+  {id: 1, content: 'item 1', start: '2013-04-20'}
+])
+
+let options = {}
+
+let timeline = new vis.Timeline(timelineDiv, items, options)
